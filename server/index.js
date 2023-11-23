3dc827f6c2f6774f819623715449d3ecfbeb2c81
HuyRuaOmG <139605984+HuyRuaOmG@users.noreply.github.com>
2023-11-23 15:53:19 +0700

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -12,15 +12,24 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 app.get('/hello', (req,res) => {
   res.json({ message: 'Hello from server!' });
 });
-const path=require('path') ;
-app.use('/api/admin',require('./api/admin.js'));
-app.use('/api/customer',require('./api/customer.js'));
-app.use('/admin',express.static(path.resolve(__dirname,'../client-admin/build')));
-app.get('admin/*',(req,res) => {
-  res.sendFile(path.resolve(__dirname,'../client-admin/build','index.html'))
+const express = require('express');
+const path = require('path');
+
+
+
+app.use('/api/admin', require('./api/admin.js'));
+app.use('/api/customer', require('./api/customer.js'));
+
+app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
+app.get('/admin/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'));
 });
-const path=require('path') ;
-app.use('/',express.static(path.resolve(__dirname,'../client-customer/build')));
-app.get('*',(req,res) => {
-  res.sendFile(path.resolve(__dirname,'../client-customer/build','index.html'));
-}) 
+
+app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
