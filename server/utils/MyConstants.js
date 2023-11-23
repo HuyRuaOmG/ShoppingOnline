e90a796bbdb4682dc5072d7d762bd0d2a2891667
HuyRuaOmG <139605984+HuyRuaOmG@users.noreply.github.com>
2023-11-23 16:50:12 +0700

    Add files via upload

--- server/utils/MyConstants.js
+++ server/utils/MyConstants.js
@@ -1,11 +1,11 @@
 const MyConstants = {
-  DB_SERVER: 'cluster01.imp2h3y.mongodb.net',
-  DB_USER: 'hotiendat',
+  DB_SERVER: 'cluster01.goeggyv.mongodb.net',
+  DB_USER: 'admin',
   DB_PASS: '123',
   DB_DATABASE: 'shoppingonline',
   JWT_SECRET: '442',
-  JWT_EXPIRES: '99999999999999999', // in milliseconds
-  EMAIL_USER: 'hotiendat1112@gmail.com', // gmail service
-  EMAIL_PASS: '<password>'
+  JWT_EXPIRES: '31565890000000', // in milliseconds
+  EMAIL_USER: 'tranquochuyomg2015@gmail.com', // gmail service
+  EMAIL_PASS: 'uuyxfxzxilviblzh'
 }
 module.exports = MyConstants;
